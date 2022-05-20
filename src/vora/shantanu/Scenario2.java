6aa3b35b208f5168aff2672e497a1750dc483981
Shantanu Vora <shantu.git@gmail.com>
2022-05-20 19:13:31 +0530

    Add constructMap method for Scenario2

--- src/vora/shantanu/Scenario2.java
+++ src/vora/shantanu/Scenario2.java
@@ -1,6 +1,7 @@
 package vora.shantanu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Scenario2 {
 	private ArrayList<ArrayList<String>> matchData;
@@ -13,5 +14,21 @@ public class Scenario2 {
 		return matchData;
 	}
 
+	private HashMap<String, Integer> constructMap() {
+		HashMap<String, Integer> map = new HashMap<>();
+
+		for(ArrayList<String> match: matchData) {
+			String key = match.get(10);
+			if(!key.equals("")) {
+				if(!map.containsKey(key)) {
+					map.put(key, 1);
+				} else {
+					map.put(key, map.get(key) + 1);
+				}
+			}
+		}
+
+		return map;
+	}
 
 }
