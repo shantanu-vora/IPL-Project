5c6277354d072c1bf94cbbdcd29412b4b426131e
Shantanu Vora <shantu.git@gmail.com>
2022-05-20 23:14:08 +0530

    Add Scenario4 class Constructor and getter methods for Scenario4

--- src/vora/shantanu/Scenario4.java
+++ src/vora/shantanu/Scenario4.java
@@ -1,6 +1,24 @@
 package vora.shantanu;
 
 
+import java.util.ArrayList;
+
 public class Scenario4 {
+	private ArrayList<ArrayList<String>> matchData;
+	private ArrayList<ArrayList<String>> deliveryData;
+
+	public Scenario4(ArrayList<ArrayList<String>> matchData, ArrayList<ArrayList<String>> deliveryData) {
+		this.matchData = matchData;
+		this.deliveryData = deliveryData;
+	}
+
+	public ArrayList<ArrayList<String>> getMatchData() {
+		return matchData;
+	}
+
+	public ArrayList<ArrayList<String>> getDeliveryData() {
+		return deliveryData;
+	}
+
 
 }
