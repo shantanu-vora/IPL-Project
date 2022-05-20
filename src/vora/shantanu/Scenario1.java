ff893d3c7b417cd9a0b765e4bea018b49cdabc00
Shantanu Vora <shantu.git@gmail.com>
2022-05-20 13:46:25 +0530

    create constructMap function

--- src/vora/shantanu/Scenario1.java
+++ src/vora/shantanu/Scenario1.java
@@ -1,6 +1,8 @@
 package vora.shantanu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Scenario1 {
 	private ArrayList<ArrayList<String>> matchData;
@@ -12,4 +14,22 @@ public class Scenario1 {
 	public ArrayList<ArrayList<String>> getMatchData() {
 		return matchData;
 	}
+
+	public void constructMap() {
+		Map<String, Integer> map = new HashMap<>();
+
+
+		for(ArrayList<String> match: matchData) {
+			String key = match.get(1);
+			if(!map.containsKey(key)) {
+				map.put(key, 1);
+			} else {
+				map.put(key, map.get(key) + 1);
+			}
+		}
+
+		System.out.println(map);
+
+	}
+
 }
