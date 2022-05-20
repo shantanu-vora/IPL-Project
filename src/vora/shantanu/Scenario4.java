83bb1a1831a0678f09da088f97a7dd603126b1e2
Shantanu Vora <shantu.git@gmail.com>
2022-05-21 03:09:39 +0530

    Add deliveriesAndRunsMap function

--- src/vora/shantanu/Scenario4.java
+++ src/vora/shantanu/Scenario4.java
@@ -2,6 +2,7 @@ package vora.shantanu;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Scenario4 {
 	private ArrayList<ArrayList<String>> matchData;
@@ -29,6 +30,28 @@ public class Scenario4 {
 		}
 
 		return matches2015;
+	}
+
+	public HashMap<String, ArrayList<Double>> deliveriesAndRunsMap(ArrayList<ArrayList<String>> matches2015) {
+		HashMap<String, ArrayList<Double>> map = new HashMap<>();
+		for(ArrayList<String> match: matches2015) {
+			String matchId = match.get(0);
+			for(ArrayList<String> delivery: deliveryData) {
+				if(delivery.get(0).equals(matchId)) {
+					String key = delivery.get(8);
+					if(!map.containsKey(key)) {
+						map.put(key, new ArrayList<>());
+						map.get(key).add(0, 1.0);
+						map.get(key).add(1, Double.parseDouble(delivery.get(17)));
+					} else {
+						map.get(key).set(0, map.get(key).get(0) + 1.0);
+						map.get(key).set(1, map.get(key).get(1) + Double.parseDouble(delivery.get(17)));
+						map.put(key, map.get(key));
+					}
+				}
+			}
+		}
+		return map;
 	}
 
 }
