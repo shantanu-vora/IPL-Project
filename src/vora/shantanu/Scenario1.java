498054ca46806b25e7ae29dc22d1bc47f85e422b
Shantanu Vora <shantu.git@gmail.com>
2022-05-20 13:44:26 +0530

    declare arrayList matchData & initialize constructor and getter

--- src/vora/shantanu/Scenario1.java
+++ src/vora/shantanu/Scenario1.java
@@ -1,4 +1,15 @@
 package vora.shantanu;
 
+import java.util.ArrayList;
+
 public class Scenario1 {
+	private ArrayList<ArrayList<String>> matchData;
+
+	public Scenario1(ArrayList<ArrayList<String>> matchData) {
+		this.matchData = matchData;
+	}
+
+	public ArrayList<ArrayList<String>> getMatchData() {
+		return matchData;
+	}
 }
