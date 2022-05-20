843c2598d83a2421ebc1a5a9c9a8f491b6de2588
Shantanu Vora <shantu.git@gmail.com>
2022-05-20 11:57:14 +0530

    Import both datasets into the Java program

--- src/vora/shantanu/Main.java
+++ src/vora/shantanu/Main.java
@@ -1,7 +1,45 @@
 package vora.shantanu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public static void main(String[] args) throws IOException {
+
+		List<List<String>> deliveryData = new ArrayList<>();
+		List<List<String>> matchData = new ArrayList<>();
+		List<String> match;
+		List<String> delivery;
+
+		FileReader matchReader = new FileReader("./matches.csv");
+		FileReader deliveryReader = new FileReader("./deliveries.csv");
+
+		BufferedReader bufferedReader = new BufferedReader(matchReader);
+		String[] matchHeaders = bufferedReader.readLine().split(",");
+//		System.out.println(Arrays.asList(matchHeaders));
+
+		String line = "";
+		while((line = bufferedReader.readLine()) != null) {
+			match = Arrays.asList((line.split(",")));
+			matchData.add(match);
+		}
+
+		bufferedReader = new BufferedReader(deliveryReader);
+		String[] deliveryHeaders = bufferedReader.readLine().split(",");
+//		System.out.println(Arrays.asList(deliveryHeaders));
+
+		while((line = bufferedReader.readLine()) != null) {
+			delivery = Arrays.asList((line.split(",")));
+			deliveryData.add(delivery);
+		}
+
+
+
+
 	}
 }
