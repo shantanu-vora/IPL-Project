5f4ac53be66f868251c0d309c71e3d42faaa2733
Shantanu Vora <shantu.git@gmail.com>
2022-05-21 13:01:24 +0530

    Refactor Scenario3 class - Add comments

--- src/vora/shantanu/Scenario3.java
+++ src/vora/shantanu/Scenario3.java
@@ -20,6 +20,10 @@ public class Scenario3 {
 		return deliveryData;
 	}
 
+	/*
+		getMatches2016() method returns a nested arrayList filtered with only
+		rows(matches) played in Season year 2016. It accesses season year by index
+	 */
 	private ArrayList<ArrayList<String>> getMatches2016() {
 		ArrayList<ArrayList<String>> matches2016 = new ArrayList<>();
 		for(ArrayList<String> match: matchData) {
@@ -31,12 +35,22 @@ public class Scenario3 {
 		return matches2016;
 	}
 
+	/*
+		constructMap() method accepts an arrayList filtered with matches played in 2016.
+		It returns a hashmap. It accesses match_id for every match and compares it with id from
+		deliveriesData list. If it matches then key is initialized with Bowling team name indexed by 3.
+		It initializes extras with index 16. Then checks if the map contains key(bowling team).
+		If not, initialize the key with extras. If yes, then just add extras to the original extras
+
+	 */
 	private HashMap<String, Integer> constructMap(ArrayList<ArrayList<String>> matches2016) {
 		HashMap<String, Integer> map = new HashMap<>();
 		for(ArrayList<String> match: matches2016) {
+			// Access match_id by index 0
 			String matchId = match.get(0);
 			for(ArrayList<String> delivery : deliveryData) {
 				if(delivery.get(0).equals(matchId)) {
+					// Access bowling team name by index 3 and extras runs by index 16
 					String key = delivery.get(3);
 					int extras = Integer.parseInt(delivery.get(16));
 					if(!map.containsKey(key)) {
