407368da9f444064d5b3412e9a7231d2045c6bd8
Shantanu Vora <shantu.git@gmail.com>
2022-05-21 12:18:02 +0530

    Add deliveriesAndRunsMap() and constructMap() method in Scenario5 class

--- src/vora/shantanu/Scenario5.java
+++ src/vora/shantanu/Scenario5.java
@@ -1,6 +1,8 @@
 package vora.shantanu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Scenario5 {
 	private ArrayList<ArrayList<String>> matchData;
@@ -28,5 +30,42 @@ public class Scenario5 {
 		}
 		return matches2017;
 	}
+
+	private HashMap<String, ArrayList<Double>> deliveriesAndRunsMap(ArrayList<ArrayList<String>> matches2017) {
+		HashMap<String, ArrayList<Double>> map = new HashMap<>();
+		for(ArrayList<String> match: matches2017) {
+			String matchId = match.get(0);
+			for(ArrayList<String> delivery: deliveryData) {
+				if(delivery.get(0).equals(matchId)) {
+					String key = delivery.get(6);
+					if(!map.containsKey(key)) {
+						map.put(key, new ArrayList<>());
+						map.get(key).add(0, 1.0);
+						map.get(key).add(1, Double.parseDouble(delivery.get(15)));
+					} else {
+						map.get(key).set(0, map.get(key).get(0) + 1.0);
+						map.get(key).set(1, map.get(key).get(1) + Double.parseDouble(delivery.get(15)));
+						map.put(key, map.get(key));
+					}
+				}
+			}
+		}
+		return map;
+	}
+
+	private HashMap<String, Double> constructMap(HashMap<String, ArrayList<Double>> map) {
+		HashMap<String, Double> strikeRateMap = new HashMap<>();
+
+		for(Map.Entry<String, ArrayList<Double>> m: map.entrySet()) {
+			Double deliveries = map.get(m.getKey()).get(0);
+			Double runs = map.get(m.getKey()).get(1);
+			Double strikeRate = (runs / deliveries) * 100.0;
+
+			strikeRateMap.put(m.getKey(), strikeRate);
+		}
+		return strikeRateMap;
+	}
+
+
 
 }
