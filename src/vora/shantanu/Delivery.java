ee94bcd605d2ae1a249012b36aa5f6aebba57985
Shantanu Vora <shantu.git@gmail.com>
2022-05-23 20:46:14 +0530

    Add POJO variable, getters and setters for both Match and Delivery classes

--- src/vora/shantanu/Delivery.java
+++ src/vora/shantanu/Delivery.java
@@ -1,2 +1,68 @@
-package vora.shantanu;public class Delivery {
+package vora.shantanu;
+
+public class Delivery {
+	private String matchId;
+	private String bowlingTeam;
+	private String extraRuns;
+	private String bowler;
+	private String totalRuns;
+	private String batter;
+
+	public String getBatter() {
+		return batter;
+	}
+
+	public void setBatter(String batter) {
+		this.batter = batter;
+	}
+
+	private String batterRuns;
+
+	public String getBatterRuns() {
+		return batterRuns;
+	}
+
+	public void setBatterRuns(String batterRuns) {
+		this.batterRuns = batterRuns;
+	}
+
+	public String getMatchId() {
+		return matchId;
+	}
+
+	public void setMatchId(String matchId) {
+		this.matchId = matchId;
+	}
+
+	public String getBowlingTeam() {
+		return bowlingTeam;
+	}
+
+	public String getBowler() {
+		return bowler;
+	}
+
+	public void setBowler(String bowler) {
+		this.bowler = bowler;
+	}
+
+	public void setBowlingTeam(String bowlingTeam) {
+		this.bowlingTeam = bowlingTeam;
+	}
+
+	public String getExtraRuns() {
+		return extraRuns;
+	}
+
+	public void setExtraRuns(String extraRuns) {
+		this.extraRuns = extraRuns;
+	}
+
+	public String getTotalRuns() {
+		return totalRuns;
+	}
+
+	public void setTotalRuns(String totalRuns) {
+		this.totalRuns = totalRuns;
+	}
 }
--- src/vora/shantanu/Match.java
+++ src/vora/shantanu/Match.java
@@ -1,4 +1,31 @@
 package vora.shantanu;
 
 public class Match {
+	private String id;
+	private String season;
+	private String winner;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSeason() {
+		return season;
+	}
+
+	public void setSeason(String season) {
+		this.season = season;
+	}
+
+	public String getWinner() {
+		return winner;
+	}
+
+	public void setWinner(String winner) {
+		this.winner = winner;
+	}
 }
